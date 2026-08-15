4a6d37738046c340af314460f648587175ce9d49
desoumya04 <13000123098@ticollege.org>
2026-08-15 12:08:52 +0530

    Update the cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import router from './routes/indiex.js';
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
 }));
 app.use(express.json());
